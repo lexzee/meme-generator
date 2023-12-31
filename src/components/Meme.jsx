b81d766b8bcde46e177a1ced516de495096a06f9
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-12-31 08:56:47 +0100

    Modified some components

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
+import memesData from '../memesData'
 
 const Meme = () => {
+  // const [memeImage, setMemeImage] = React.useState('')
+
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: ""
+  })
+  const [allMemeImages, setAllMemeImages] =React.useState(memesData.data.memes)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const randIndex = Math.floor(Math.random() * allMemeImages.length)
+    console.log(allMemeImages[randIndex].url)
+    setMeme(prevState =>({
+      ...prevState, randomImage: allMemeImages[randIndex].url
+    }))
+  }
+
+
   return (
     <main>
       <form className="form">
@@ -14,11 +34,15 @@ const Meme = () => {
           className='form--input'
           placeholder="Bottom text"
         />
-        {/* <input className='form--button' type='submit' value="Get a new meme image 🖼" /> */}
-        <button className='form--button'>Get a new meme image 🖼</button>
+        <button
+          className='form--button'
+          onClick={handleSubmit}
+        >Get a new meme image 🖼</button>
+      {meme.randomImage && <img className='meme--image' src={meme.randomImage} alt='meme' />}
       </form>
     </main>
   )
 }
+
 
 export default Meme
